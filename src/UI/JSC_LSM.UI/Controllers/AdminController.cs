35bf465b54a982c02c3c1eb97896164ee48420c9
VishramS <vishram.sawant@neosoftmail.com>
2021-10-19 14:42:00 +0530

    Added Profile View

---
+++ src/UI/JSC_LSM.UI/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JSC_LSM.UI.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+    }
+}
