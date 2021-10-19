24295114628d575468a262067bc819c2b3aad584
Shivani422 <shivani.goswami@neosoftmail.com>
2021-10-19 14:49:18 +0530

    Forgot Password Designed the screen.

---
+++ src/UI/JSC_LSM.UI/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JSC_LSM.UI.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+    }
+}
