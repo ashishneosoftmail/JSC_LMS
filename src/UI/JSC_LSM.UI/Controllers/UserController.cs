81ebdb231ee8a65f5a65e96ccade5953e4761e94
Harsh Chheda <92707209+harshc21@users.noreply.github.com>
2021-10-20 20:17:55 +0530

    Added Add Student Design

---
+++ src/UI/JSC_LSM.UI/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JSC_LSM.UI.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddUser()
+        {
+            return View();
+        }
+        public IActionResult AddStudent()
+        {
+            return View();
+        }
+        public IActionResult AddParents()
+        {
+            return View();
+
+        }
+    }
+}
