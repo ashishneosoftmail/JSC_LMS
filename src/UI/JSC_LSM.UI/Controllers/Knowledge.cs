f33816624ad6e5ae465a1c4994e1323d63d6ea67
neogit123 <patil.nivrutti@neosoftmail.com>
2021-10-20 19:49:21 +0530

    Completed add Knowledge base and Add School

---
+++ src/UI/JSC_LSM.UI/Controllers/Knowledge.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JSC_LSM.UI.Controllers
+{
+    public class Knowledge : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult KnowledgeBase()
+        {
+            return View();
+        }
+    }
+}
