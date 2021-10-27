244b5c85c62171017bf45259afa397f0108873f4
SushmaJP02 <sushma.p@neosoftmail.com>
2021-10-27 10:42:07 +0530

    Added Teacher controller and Manage student user page

---
+++ src/UI/JSC_LSM.UI/Controllers/TeacherController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JSC_LSM.UI.Controllers
+{
+    public class TeacherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ManageStudentUsers()
+        {
+            return View();
+        }
+        
+    }
+}
