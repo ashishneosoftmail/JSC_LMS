1e847a25705e2ae8281791d7a9754b7ae822ec03
Shivani422 <shivani.goswami@neosoftmail.com>
2021-10-30 19:21:09 +0530

    Completed GetById For Institute

--- src/Core/JSC_LMS.Application/Features/Institutes/Queries/GetInstituteById/GetInstituteQueryHandler.cs
+++ src/Core/JSC_LMS.Application/Features/Institutes/Queries/GetInstituteById/GetInstituteQueryHandler.cs
@@ -1,8 +1,12 @@
 ﻿using AutoMapper;
+using JSC_LMS.Application.CommonDtos;
+using JSC_LMS.Application.Contracts.Identity;
 using JSC_LMS.Application.Contracts.Persistence;
 using JSC_LMS.Application.Features.Institutes.Queries.GetInstituteList;
+using JSC_LMS.Application.Features.Principal.Queries.GetPrincipalById;
 using JSC_LMS.Application.Responses;
 using MediatR;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,25 +15,74 @@ using System.Threading.Tasks;
 
 namespace JSC_LMS.Application.Features.Institutes.Queries.GetInstituteById
 {
-    
-    public class GetInstituteQueryHandler : IRequestHandler<GetInstituteQuery, Response<GetInstituteListVm>>
-    {
-        private readonly IMapper _mapper;
-        private readonly IInstituteRepository _instituteRepository;
 
-        public GetInstituteQueryHandler(IMapper mapper, IInstituteRepository instituteRepository)
+    //public class GetInstituteQueryHandler : IRequestHandler<GetInstituteQuery, Response<GetInstituteListVm>>
+    //{
+    //    private readonly IMapper _mapper;
+    //    private readonly IInstituteRepository _instituteRepository;
+
+    //    public GetInstituteQueryHandler(IMapper mapper, IInstituteRepository instituteRepository)
+    //    {
+    //        _mapper = mapper;
+    //        _instituteRepository = instituteRepository;
+    //    }
+
+    //    public async Task<Response<GetInstituteListVm>> Handle(GetInstituteQuery request, CancellationToken cancellationToken)
+    //    {
+    //        var list = await _instituteRepository.GetByIdAsync(request.Id);
+    //        var instituteList = _mapper.Map<GetInstituteListVm>(list);
+    //        return new Response<GetInstituteListVm>(instituteList, "success");
+    //    }
+
+
+    //}
+
+    public class GetInstituteByIdQueryHandler : IRequestHandler<GetInstituteQuery, Response<GetInstituteByIdVm>>
+    {
+        private readonly IInstituteRepository _instituteRepository;        
+        private readonly IAuthenticationService _authenticationService;
+        private readonly IMapper _mapper;
+        private readonly ILogger _logger;
+        public GetInstituteByIdQueryHandler(IMapper mapper, IInstituteRepository instituteRepository, IAuthenticationService authenticationService, ILogger<GetPrincipalByIdQueryHandler> logger)
         {
             _mapper = mapper;
             _instituteRepository = instituteRepository;
+            _logger = logger;
+            _authenticationService = authenticationService;
         }
 
-        public async Task<Response<GetInstituteListVm>> Handle(GetInstituteQuery request, CancellationToken cancellationToken)
+        public async Task<Response<GetInstituteByIdVm>> Handle(GetInstituteQuery request, CancellationToken cancellationToken)
         {
-            var list = await _instituteRepository.GetByIdAsync(request.Id);
-            var instituteList = _mapper.Map<GetInstituteListVm>(list);
-            return new Response<GetInstituteListVm>(instituteList, "success");
+            _logger.LogInformation("Handle Initiated");
+            Response<GetInstituteByIdVm> responseData = new Response<GetInstituteByIdVm>();
+            var institute = await _instituteRepository.GetByIdAsync(request.Id);
+            if (institute == null)
+            {
+                responseData.Succeeded = true;
+                responseData.Message = "Data Doesn't Exist";
+                responseData.Data = null;
+                return responseData;
+            }
+            var user = await _authenticationService.GetUserById(institute.UserId);
+            var instituteData = new GetInstituteByIdVm()
+            {
+                Id = institute.Id,
+                AddressLine1 = institute.AddressLine1,
+                AddressLine2 = institute.AddressLine2,
+                Mobile = institute.Mobile,
+                Username = user.Username,
+                Email = user.Email,
+                IsActive = institute.IsActive,
+                ContactPerson = institute.ContactPerson,
+                CreatedDate = (DateTime)institute.CreatedDate,
+                City = _mapper.Map<CityDto>(institute.City),
+                State = _mapper.Map<StateDto>(institute.State),
+                Zip = _mapper.Map<ZipDto>(institute.Zip),
+               
+            };
+            _logger.LogInformation("Hanlde Completed");
+            return new Response<GetInstituteByIdVm>(instituteData, "success");
         }
-
 
     }
 }
--- src/UI/JSC_LSM.UI/Services/Repositories/InstituteRepository.cs
+++ src/UI/JSC_LSM.UI/Services/Repositories/InstituteRepository.cs
@@ -8,4 +8,46 @@ namespace JSC_LSM.UI.Services.Repositories
     public class InstituteRepository
     {
     }
+
+    //public class InstituteRepository : IUserRepository
+    //{
+    //    private APIRepository _aPIRepository;
+    //    private APICommunicationResponseModel<string> _oApiResponse;
+    //    private string _sToken = string.Empty;
+    //    private readonly IOptions<ApiBaseUrl> _apiBaseUrl;
+    //    private readonly IConfiguration _configuration;
+
+    //    public UserRepository()
+    //    {
+
+    //    }
+
+    //    public async Task<AuthenticationResponseModel> UserAuthenticate(AuthenticationRequest authenticateRequest)
+    //    {
+    //        AuthenticationResponseModel authenticationResponseModel = null;
+    //        _aPIRepository = new APIRepository(_configuration);
+
+    //        _oApiResponse = new APICommunicationResponseModel<string>();
+    //        var json = JsonConvert.SerializeObject(authenticateRequest, Formatting.Indented);
+    //        byte[] content = Encoding.ASCII.GetBytes(json);
+    //        var bytes = new ByteArrayContent(content);
+
+    //        _oApiResponse = await _aPIRepository.APICommunication(UrlHelper.UserAuthenticate, HttpMethod.Post, bytes, _sToken);
+    //        if (_oApiResponse.data != null)
+    //        {
+    //            authenticationResponseModel = JsonConvert.DeserializeObject<AuthenticationResponseModel>(_oApiResponse.data);
+    //            if (authenticationResponseModel.isAuthenticated)
+    //            {
+    //                authenticationResponseModel.isSuccess = true;
+    //            }
+    //            else
+    //            {
+    //                authenticationResponseModel.isSuccess = false;
+    //            }
+    //        }
+
+    //        return authenticationResponseModel;
+
+    //    }
+    //}
 }
