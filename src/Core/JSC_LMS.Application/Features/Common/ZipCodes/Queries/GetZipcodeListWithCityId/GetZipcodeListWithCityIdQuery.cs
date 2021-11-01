2c37c89771b20208ebaa544404fdae7eea79b32d
Shivani422 <shivani.goswami@neosoftmail.com>
2021-11-01 13:26:14 +0530

    completed get Zip  Queries

---
+++ src/Core/JSC_LMS.Application/Features/Common/ZipCodes/Queries/GetZipcodeListWithCityId/GetZipcodeListWithCityIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using JSC_LMS.Application.Responses;
+using JSC_LMS.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSC_LMS.Application.Features.Common.ZipCodes.Queries.GetZipcodeListWithCityId
+{
+    public class GetZipcodeListWithCityIdQuery : IRequest<Response<IEnumerable<Zip>>>
+    {
+        public int CityId { get; set; }
+    }
+}
