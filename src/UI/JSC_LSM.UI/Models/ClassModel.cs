61472bccc0a0691aa7dfeaa9094967ad06a8e71e
VishramS <vishram.sawant@neosoftmail.com>
2021-11-02 18:07:01 +0530

    creating Class model

---
+++ src/UI/JSC_LSM.UI/Models/ClassModel.cs
@@ -0,0 +1,14 @@
+﻿using JSC_LMS.Application.Features.Class.Commands.CreateClass;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JSC_LSM.UI.Models
+{
+    public class ClassModel : CreateClassDto
+    {
+        public List<SelectListItem> Schools { get; set; }
+    }
+}
---
+++ src/UI/JSC_LSM.UI/Services/IRepositories/IClassRepository.cs
@@ -0,0 +1,13 @@
+﻿using JSC_LMS.Application.Features.Class.Commands.CreateClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JSC_LSM.UI.Services.IRepositories
+{
+  public  interface IClassRepository
+    {
+                Task<ClassResponseModel> AddNewClass(CreateClassDto createClassDto);
+    }
+}
