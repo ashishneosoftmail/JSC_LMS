cc252502bf7c3f08f6a5be33beaecba71df66fe4
Shivani422 <shivani.goswami@neosoftmail.com>
2021-11-03 19:59:07 +0530

    Pagination at  API

---
+++ src/Core/JSC_LMS.Application/Features/Institutes/Queries/GetInstituteByPagination/GetInstituteByPaginationQuery.cs
@@ -0,0 +1,15 @@
+﻿using JSC_LMS.Application.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSC_LMS.Application.Features.Institutes.Queries.GetInstituteByPagination
+{
+   
+    public class GetInstituteByPaginationQuery : IRequest<Response<GetInstituteListByPaginationResponse>>
+    {
+        public int page { get; set; }
+        public int size { get; set; }
+    }
+}
