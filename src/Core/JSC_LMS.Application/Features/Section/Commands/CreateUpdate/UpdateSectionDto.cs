a1a1eb1b8654e0a1acb5937a8f0d87ed79166c85
VishramS <vishram.sawant@neosoftmail.com>
2021-11-11 13:17:33 +0530

    Done with Subject .Working on validation

--- src/Core/JSC_LMS.Application/Features/Section/Commands/CreateUpdate/UpdateSectionDto.cs
+++ src/Core/JSC_LMS.Application/Features/Section/Commands/CreateUpdate/UpdateSectionDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace JSC_LMS.Application.Features.Section.Commands.CreateUpdate
@@ -7,9 +8,16 @@ namespace JSC_LMS.Application.Features.Section.Commands.CreateUpdate
   public  class UpdateSectionDto
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "School Is Required")]
         public int SchoolId { get; set; }
+
+        [Required(ErrorMessage = "Class Is Required")]
         public int ClassId { get; set; }
 
+
+        [StringLength(100, ErrorMessage = "Section Name should not be more than 50 characters")]
+        [Required(ErrorMessage = "Section Name Is Required")]
         public string SectionName { get; set; }
         public bool IsActive { get; set; }
     }
--- src/Core/JSC_LMS.Application/Models/Authentication/RegistrationResponse.cs
+++ src/Core/JSC_LMS.Application/Models/Authentication/RegistrationResponse.cs
@@ -3,5 +3,6 @@ namespace JSC_LMS.Application.Models.Authentication
     public class RegistrationResponse
     {
         public string UserId { get; set; }
+        public string ClassId { get; set; }
     }
 }
