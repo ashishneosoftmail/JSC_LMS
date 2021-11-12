e99784205dda9f718afeb33c13c236b4edc2de89
VishramS <vishram.sawant@neosoftmail.com>
2021-11-12 11:39:23 +0530

    Done with All three class section and subject module with validation

--- src/Core/JSC_LMS.Application/Contracts/Persistence/ISectionRepository.cs
+++ src/Core/JSC_LMS.Application/Contracts/Persistence/ISectionRepository.cs
@@ -2,12 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.Threading.Tasks;
 
 namespace JSC_LMS.Application.Contracts.Persistence
 {
     public interface ISectionRepository : IAsyncRepository<Section>
     {
-
+        Task<bool> IsSectionName(string SectionName,int SchoolId,int ClassId);
     }
 }
