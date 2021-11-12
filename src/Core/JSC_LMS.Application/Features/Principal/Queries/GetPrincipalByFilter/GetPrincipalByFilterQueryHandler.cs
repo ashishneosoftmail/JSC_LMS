88c8892dfa5f80ea0c4c43dad7223492f0534f4f
Harsh Chheda <92707209+harshc21@users.noreply.github.com>
2021-11-12 15:16:18 +0530

    Done With Search Filter

--- src/Core/JSC_LMS.Application/Features/Principal/Queries/GetPrincipalByFilter/GetPrincipalByFilterQueryHandler.cs
+++ src/Core/JSC_LMS.Application/Features/Principal/Queries/GetPrincipalByFilter/GetPrincipalByFilterQueryHandler.cs
@@ -36,20 +36,57 @@ namespace JSC_LMS.Application.Features.Principal.Queries.GetPrincipalByFilter
         {
             _logger.LogInformation("Handle Initiated");
             var allPrincipal = await _principalRepository.ListAllAsync();
-            var searchFilter = (allPrincipal.Where<JSC_LMS.Domain.Entities.Principal>(x => (x.Name == request.PrincipalName) && (x.CreatedDate?.ToShortDateString() == request.CreatedDate.ToShortDateString()) && (x.IsActive == request.IsActive)).Select(x => (x)));
-            Response<IEnumerable<GetPrincipalByFilterDto>> responseData = new Response<IEnumerable<GetPrincipalByFilterDto>>();
-            if (searchFilter.Count() < 1)
+            var searchFilter = (allPrincipal.Where<JSC_LMS.Domain.Entities.Principal>(x => (x.Name == request.PrincipalName) || (x.CreatedDate?.ToShortDateString() == request.CreatedDate.ToShortDateString()) || (x.IsActive == request.IsActive)).Select(x => (x)));
+
+            if (request.PrincipalName != "Select Principal")
             {
-                responseData.Succeeded = true;
-                responseData.Message = "Data Doesn't Exist";
-                responseData.Data = null;
-                return responseData;
+                allPrincipal = allPrincipal.Where<JSC_LMS.Domain.Entities.Principal>(x => (x.Name == request.PrincipalName)).ToList();
             }
-            List<GetPrincipalByFilterDto> principalList = new List<GetPrincipalByFilterDto>();
-            foreach (var principal in searchFilter)
+            if (request.CreatedDate.ToShortDateString() != "01-01-0001")
             {
-                var school = (await _schoolRepository.GetByIdAsync(principal.SchoolId)).SchoolName == request.SchoolName;
-                if (school)
+                allPrincipal = allPrincipal.Where<JSC_LMS.Domain.Entities.Principal>(x => x.CreatedDate?.ToShortDateString() == request.CreatedDate.ToShortDateString()).ToList();
+            }
+            if (request.IsActive)
+            {
+                allPrincipal = allPrincipal.Where<JSC_LMS.Domain.Entities.Principal>(x => x.IsActive == request.IsActive).ToList();
+            }
+            else
+            {
+                allPrincipal = allPrincipal.Where<JSC_LMS.Domain.Entities.Principal>(x => x.IsActive == request.IsActive).ToList();
+            }
+            Response<IEnumerable<GetPrincipalByFilterDto>> responseData = new Response<IEnumerable<GetPrincipalByFilterDto>>();
+            List<GetPrincipalByFilterDto> principalList = new List<GetPrincipalByFilterDto>();
+            foreach (var principal in allPrincipal)
+            {
+                if (request.SchoolName != "Select School")
+                {
+                    var school = (await _schoolRepository.GetByIdAsync(principal.SchoolId)).SchoolName == request.SchoolName;
+                    if (school)
+                    {
+                        var user = await _authenticationService.GetUserById(principal.UserId);
+                        principalList.Add(new GetPrincipalByFilterDto()
+                        {
+                            Id = principal.Id,
+                            AddressLine1 = principal.AddressLine1,
+                            AddressLine2 = principal.AddressLine2,
+                            Mobile = principal.Mobile,
+                            Username = user.Username,
+                            Email = user.Email,
+                            IsActive = principal.IsActive,
+                            Name = principal.Name,
+                            CreatedDate = (DateTime)principal.CreatedDate,
+                            City = _mapper.Map<CityDto>(principal.City),
+                            State = _mapper.Map<StateDto>(principal.State),
+                            Zip = _mapper.Map<ZipDto>(principal.Zip),
+                            School = new SchoolFilterDto()
+                            {
+                                Id = principal.SchoolId,
+                                SchoolName = (await _schoolRepository.GetByIdAsync(principal.SchoolId)).SchoolName
+                            }
+                        });
+                    }
+                }
+                else
                 {
                     var user = await _authenticationService.GetUserById(principal.UserId);
                     principalList.Add(new GetPrincipalByFilterDto()
@@ -72,14 +109,8 @@ namespace JSC_LMS.Application.Features.Principal.Queries.GetPrincipalByFilter
                             SchoolName = (await _schoolRepository.GetByIdAsync(principal.SchoolId)).SchoolName
                         }
                     });
+
                 }
-            }
-            if (principalList.Count() < 1)
-            {
-                responseData.Succeeded = true;
-                responseData.Message = "Data Doesn't Exist";
-                responseData.Data = null;
-                return responseData;
             }
             _logger.LogInformation("Hanlde Completed");
             return new Response<IEnumerable<GetPrincipalByFilterDto>>(principalList, "success");
