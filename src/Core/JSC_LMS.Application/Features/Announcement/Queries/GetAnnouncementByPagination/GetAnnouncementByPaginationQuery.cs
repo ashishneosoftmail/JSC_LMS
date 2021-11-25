598907c39ddc63bb867ed7999e8fd2e6759b91a5
Shivani422 <shivani.goswami@neosoftmail.com>
2021-11-25 13:06:18 +0530

    local commit

--- src/Core/JSC_LMS.Application/Features/Announcement/Queries/GetAnnouncementByPagination/GetAnnouncementByPaginationQuery.cs
+++ src/Core/JSC_LMS.Application/Features/Announcement/Queries/GetAnnouncementByPagination/GetAnnouncementByPaginationQuery.cs
@@ -7,7 +7,8 @@ using System.Text;
 
 namespace JSC_LMS.Application.Features.Announcement.Queries.GetAnnouncementByPagination
 {
-    public class GetAnnouncementByPaginationQuery : IRequest<Response<GetAnnouncementByPaginationResponse>>
+    
+    public class GetAnnouncementByPaginationQuery : IRequest<Response<IEnumerable<GetAnnouncementListByPaginationDto>>>
     {
         public int page { get; set; }
         public int size { get; set; }
--- src/UI/JSC_LSM.UI/Controllers/TeacherController.cs
+++ src/UI/JSC_LSM.UI/Controllers/TeacherController.cs
@@ -29,8 +29,9 @@ namespace JSC_LSM.UI.Controllers
         private readonly ISectionRepository _sectionRepository;
         private readonly ISubjectRepository _subjectRepository;
         private readonly IClassRepository _classRepository;
+        private readonly IAnnouncementRepository _announcementRepository;
 
-        public TeacherController(IStateRepository stateRepository, ISchoolRepository schoolRepository, JSC_LSM.UI.Common.Common common, IOptions<ApiBaseUrl> apiBaseUrl, ITeacherRepository teacherRepository , IClassRepository classRepository, ISectionRepository sectionRepository , ISubjectRepository subjectRepository)
+        public TeacherController(IStateRepository stateRepository, ISchoolRepository schoolRepository, JSC_LSM.UI.Common.Common common, IOptions<ApiBaseUrl> apiBaseUrl, ITeacherRepository teacherRepository , IClassRepository classRepository, ISectionRepository sectionRepository , ISubjectRepository subjectRepository , IAnnouncementRepository announcementRepository)
         {
             _stateRepository = stateRepository;
             _teacherRepository = teacherRepository;
@@ -40,6 +41,7 @@ namespace JSC_LSM.UI.Controllers
             _classRepository = classRepository;
             _sectionRepository = sectionRepository;
             _subjectRepository = subjectRepository;
+            _announcementRepository = announcementRepository;
 
         }
 
@@ -511,10 +513,41 @@ namespace JSC_LSM.UI.Controllers
 
         }
 
-        public IActionResult ManageAnnouncement()
+       
+        [HttpGet]
+        public async Task<IActionResult> ManageAnnouncement(int page = 1, int size = 5)
         {
-            return View();
+
+            int recsCount = (await _announcementRepository.GetAnnouncementList()).data.Count();
+            if (page < 1)
+                page = 1;
+            var pager = new Pager(recsCount, page, size);
+            ViewBag.Pager = pager;
+            ManageAnnouncementModel model = new ManageAnnouncementModel();
+            model.Pager = pager;
+            model.Classes = await _common.GetClass();
+            model.Sections = await _common.GetSection();
+            model.Subjects = await _common.GetSubject();
+            var paginationData = await _announcementRepository.GetAnnouncementListByPagination(page, size);
+            List<AnnouncementPagination> pagedData = new List<AnnouncementPagination>();
+            foreach (var data in paginationData.data)
+            {
+                pagedData.Add(new AnnouncementPagination()
+                {
+                    Id = data.Id,
+                   AnnouncementTitle = data.AnnouncementTitle,
+                   AnnouncementContent = data.AnnouncementContent,
+                    Class = data.Class,
+                    Section = data.Section,
+                    Subject = data.Subject,
+                    CreatedDate = data.CreatedDate,
+
+                });
+            }
+            model.AnnouncementPagination = pagedData;
+            return View(model);
         }
+
 
     }
 }
