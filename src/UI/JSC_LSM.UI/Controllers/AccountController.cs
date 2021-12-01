686a1254b9f14c0c19b1b0e6c1ffddcf50f38759
Harsh Chheda <92707209+harshc21@users.noreply.github.com>
2021-12-01 09:55:18 +0530

    Done with the teacher information page

--- src/UI/JSC_LSM.UI/Controllers/AccountController.cs
+++ src/UI/JSC_LSM.UI/Controllers/AccountController.cs
@@ -119,8 +119,8 @@ namespace JSC_LSM.UI.Controllers
                             else if (authenticationResponseModel.userDetails.Role.RoleName == "Teacher")
                             {
                                 return RedirectToAction("Index", "Teacher");
-
-                            else if(authenticationResponseModel.userDetails.Role.RoleName == "Parent")
+                            }
+                            else if (authenticationResponseModel.userDetails.Role.RoleName == "Parent")
                             {
                                 return RedirectToAction("Index", "Parent");
 
