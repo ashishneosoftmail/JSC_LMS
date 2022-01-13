5de56d86cf59709f59b2c7e11304434fe3a6dd00
vishramsa <49416183+vishramsa@users.noreply.github.com>
2022-01-13 12:59:23 +0530

    Get method for Add feedback done

--- src/Core/JSC_LMS.Application/Features/Feedback/Commands/CreateFeedback/CreateFeedbackDto.cs
+++ src/Core/JSC_LMS.Application/Features/Feedback/Commands/CreateFeedback/CreateFeedbackDto.cs
@@ -10,29 +10,29 @@ namespace JSC_LMS.Application.Features.Feedback.Commands.CreateFeedback
     public class CreateFeedbackDto
     {
         public int SchoolId { get; set; }
-        [Required(ErrorMessage = "Feedback Is Required")]
+   
         public int FeedbackTitleId { get; set; }
-        [Required(ErrorMessage = "Student Is Required")]
+     
         public int StudentId { get; set; }
-        [Required(ErrorMessage = "Parent Is Required")]
+     
         public int ParentId { get; set; }
         [Required(ErrorMessage = "Subject Is Required")]
 
         public int SubjectId { get; set; }
-        [Required(ErrorMessage = "Section Is Required")]
+
         public int SectionId { get; set; }
-        [Required(ErrorMessage = "Class Is Required")]
+ 
         public int ClassId { get; set; }
 
 
-        [Required(ErrorMessage = "FeedbackType Is Required")]
+  
         public string FeedbackType { get; set; }
  
-        [Required(ErrorMessage = "FeedbackComments Is Required")]
+     
 
         public string FeedbackComments { get; set; }
 
-        [Required(ErrorMessage = "SendDate Is Required")]
+   
         public DateTime SendDate { get; set; }
         public bool IsActive { get; set; }
 
